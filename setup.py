06544a7b9a1a649a7d4b09d869baa76999ef242b
Oscar Blanco <oscarbc@me.com>
2018-12-17 03:01:02 +0100

    Update setup v2

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@
 
 # https://github.com/kennethreitz/setup.py
 
+# Note: To use the "upload" functionality of this file, you must:
+#   $ pip install twine
+
 import io
 import os
 import sys
@@ -28,7 +31,8 @@ REQUIRES = [
 
 # What packages are optional?
 DEV_REQUIRES = [
-    "flake8"
+    "flake8",
+    "twine"
 ]
 
 # The rest you shouldn"t have to touch too much :)
@@ -54,6 +58,45 @@ if not VERSION:
 else:
     about["__version__"] = VERSION
 
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
     name=NAME,
     version=about["__version__"],
@@ -83,5 +126,9 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy"
-    ]
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        "upload": UploadCommand,
+    },
 )
